8f534e914f0306d32138c3b0aac9bbef34fcd519
Marc <marc@cardinalco.email>
2023-11-16 12:44:06 -0500

    Update form and JS

--- assets/subscription-addtocart.js
+++ assets/subscription-addtocart.js
@@ -1,49 +1,62 @@
-const rechargeButtons = document.querySelectorAll('.subscription__btn');
-const addToCartButton = document.getElementById('add-to-cart-button');
-const rechargeInput = document.getElementById('Recharge');
+/**
+ * Add subscriptions to cart
+ */
+function addSubscriptionsToCart() {
+  const rechargeButtons = document.querySelectorAll('.subscription__btn');
+  const addToCartButton = document.getElementById('add-to-cart-button');
 
-rechargeButtons.forEach(button => {
-  button.addEventListener('click', function () {
-    const selectedSubinterval = this.getAttribute('data-subinterval');
-    rechargeInput.value = selectedSubinterval;
-    console.log(selectedSubinterval)
+  let selectedSellingPlanID = null;
+  let selectedVariantID = null;
+
+  // Disable add to cart button by default
+  addToCartButton.disabled = true;
+
+  if (!rechargeButtons) return;
+  rechargeButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+      const currentTarget = e.target;
+      selectedSellingPlanID = currentTarget.dataset.sellingPlanId;
+      selectedVariantID = currentTarget.dataset.variantId;
+
+      // Enable add to cart button when a recharge button is clicked
+      addToCartButton.disabled = false;
+    });
   });
-});
 
-addToCartButton.addEventListener('click', function () {
-  addToCart();
-});
+  addToCartButton.addEventListener('click', () => { 
+    addToCart(selectedSellingPlanID, selectedVariantID);
+  });
 
-function addToCart() {
-  const variantId = "41870385610892";
-  const quantity = 1;
-  const frequency = "1";
-  const unitType = "Months";
+  /**
+   * Add selected subscription to cart
+   */
+  function addToCart(sellingPlanID, variantID) {
+    if (sellingPlanID && variantID) {
+      const data = {
+        "id": variantID,
+        "quantity": 1,
+        "selling_plan": sellingPlanID
+      };
 
-  let data;
-  data = {
-    "id": variantId,
-    "quantity": quantity,
-    "properties": {
-      "shipping_interval_frequency": frequency,
-      "shipping_interval_unit_type": unitType
+      fetch('/cart/add.js', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Requested-With': 'xmlhttprequest'
+        },
+      })
+        .then(response => response.json())
+        .then(data => {
+          window.location.href = '/cart';
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    } else {
+      console.error('Selling Plan ID or Variant ID is missing.');
     }
   }
-
-  fetch('/cart/add.js', {
-    method: 'POST',
-    body: JSON.stringify(data),
-    headers: {
-      'Content-Type': 'application/json',
-      'X-Requested-With': 'xmlhttprequest'
-    },
-  })
-    .then(response => response.json())
-    .then(data => {
-      console.log('hey', data);
-      // window.location.href = '/cart';
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
 }
+
+addSubscriptionsToCart();
